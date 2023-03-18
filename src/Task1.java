ac3228968304d2cb7d19dee5290e7bc6f7214b11
Ilya Voinkov <voinkov.ilya@yandex.ru>
2023-03-18 18:35:44 +0300

    fix

--- src/Task1.java
+++ src/Task1.java
@@ -1,28 +1,25 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.concurrent.ArrayBlockingQueue;
 
 public class Task1 {
     public static void main(String[] args) {
-        Deque<Integer> deque = new ArrayDeque<>(10);
-        Thread thread1 = new Thread() {
+        ArrayBlockingQueue queue = new ArrayBlockingQueue<>(10);
+        Thread thread1 = new Thread(new Runnable() {
+
             public void run() {
                 double tmp;
                 while (true) {
                     try {
-                        Thread.sleep(100);
                         tmp = Math.random() * 100;
-                        deque.addLast((int) tmp);
-                        if (deque.size() > 10) {
-                            deque.removeFirst();
-                            System.out.println("удалено");
-                        }
-                        System.out.println(deque);
+                        Thread.sleep(100);
+                        queue.put((int) tmp);
+
                     } catch (InterruptedException e) {
                     }
                 }
             }
-        };
-        Thread thread2 = new Thread() {
+        });
+        Thread thread2 = new Thread(new Runnable() {
+
             public void run() {
                 double tmp;
                 while (true) {
@@ -31,15 +28,15 @@ public class Task1 {
                         tmp = Math.random() * 10;
                         System.out.println((int)tmp);
                         if ((int)tmp == 5) {
-                            deque.removeFirst();
+                            queue.remove(0);
                             System.out.println("удалено из за числа 5");
                         }
-                    } catch (InterruptedException e) {
+                    } catch (InterruptedException e  ) {
                         throw new RuntimeException(e);
                     }
                 }
             }
-        };
+        });
         thread1.start();
         thread2.start();
     }
